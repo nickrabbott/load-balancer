5dd13fc024a3a0a683312591103dbe37690ac394
Nick Abbott <nick.rabbott@gmail.com>
2024-04-06 22:22:21 -0500

    completed a simple round robin

--- load-balancer.go
+++ load-balancer.go
@@ -10,7 +10,26 @@ import (
 	"os"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+var LAST_USED bool
+
+func proxyGet(r *http.Request) (*http.Response, error) {
+	var url string
+	if LAST_USED {
+		url = fmt.Sprintf("http://apache1%s", r.URL.Path)
+	} else {
+		url = fmt.Sprintf("http://apache2%s", r.URL.Path)
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		msg := "Failed to proxy GET request for %s"
+		log.Printf(msg, url)
+		return nil, err
+	}
+	LAST_USED = !LAST_USED // Server the other one next time
+	return resp, nil
+}
+
+func proxyRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println("got / request")
 	res, err := httputil.DumpRequest(r, true)
 	if err != nil {
@@ -18,14 +37,35 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println(string(res))
-	io.WriteString(w, "load balancer\n")
-	w.WriteHeader(200)
+	// io.WriteString(w, "load balancer\n")
+	// w.WriteHeader(200)
+	switch r.Method {
+	case "GET":
+		getResp, getErr := proxyGet(r)
+		if getErr != nil {
+			log.Println(getErr)
+			w.WriteHeader(502)
+			io.WriteString(w, getErr.Error())
+		} else {
+        	w.WriteHeader(getResp.StatusCode)
+        	// Get the response body
+        	body, err := io.ReadAll(getResp.Body)
+        	if err != nil {
+        	        msg := "Failed streaming response body"
+        	        log.Println(msg)
+        	        return
+        	}
+        	w.Write(body)
+		}
+	default:
+		log.Printf("%v method not implemented", r.Method)
+	}
 }
 
 func main() {
 	serverPort := os.Getenv("SERVER_PORT")
 
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", proxyRequest)
 
 	log.Printf("Server Port: %s\n", serverPort)
 
